bf7038978813ffaa38d67b70de4285441848964f
malik nihad <maliknihad04@gmail.com>
2025-12-17 14:00:58 +0000

    db updated

--- restaurant_db.sql
+++ restaurant_db.sql
@@ -1,12 +1,25 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-DROP TABLE IF EXISTS `restaurant`;
 CREATE TABLE `restaurant` (
   `meal` varchar(255) DEFAULT NULL,
   `best_restaurant` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-LOCK TABLES `restaurant` WRITE;
-INSERT INTO `restaurant` VALUES ('burger','Super Burger'),('pizza','Italiano Pizza'),('shawarma','Shawarma King'),('sushi','Tokyo House'),('burger','Burger House'),('pizza','Italiano Pizzeria'),('shawarma','Shawarma King'),('sushi','Tokyo Sushi'),('fried chicken','KFC'),('pasta','Little Italy');
-UNLOCK TABLES;
+INSERT INTO `restaurant` (`meal`, `best_restaurant`) VALUES
+('burger', 'Super Burger'),
+('pizza', 'Italiano Pizza'),
+('shawarma', 'Shawarma King'),
+('sushi', 'Tokyo House'),
+('burger', 'Burger House'),
+('pizza', 'Italiano Pizzeria'),
+('shawarma', 'Shawarma King'),
+('sushi', 'Tokyo Sushi'),
+('fried chicken', 'KFC'),
+('pasta', 'Little Italy');
+
+COMMIT;
+
 
 
