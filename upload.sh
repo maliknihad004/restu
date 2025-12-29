a8d7a1c3430f97dea78acd5667071d8131f3364d
mohammad <mohammadjamaldaraghmeh@gmail.com>
2025-12-29 11:47:58 +0000

    updates

--- upload.sh
+++ upload.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-read -p "Enter  commit message: " message
+read -p "Enter commit message: " message
+read -p "Enter branch to push to: " branch
 git add .
 git commit -m "$message"
-git push origin main
-echo "Code Uploaded Successfully!"
+git push origin "$branch"
+echo "Code uploaded successfully to branch '$branch'!"
